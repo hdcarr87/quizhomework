9e763c2500a51428555694442bd8693c874f5544
hdcarr87 <hdmanley87@gmail.com>
2020-01-30 16:28:23 -0700

    files added

---
+++ code.js
@@ -0,0 +1,20 @@
+var questions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if/else statemtn is enclosed within ______.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "What is the correct place to insert a JavaScript within the html?",
+        choices: ["The <head> or the <body>", "the <body>", "the <head>", "We do not need to insert, it will link automatically"],
+        answer: "The <head> or the <body>"
+    }
+];
+
+
+
